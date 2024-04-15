833c4c7bb6726b60d95a75f80b5eb08ea5744903
Rodrigo Escobar <rodrigoescno2010@gmail.com>
2024-04-15 09:36:24 -0600

    Agregar archivos de proyecto.

---
+++ Lab9parcial/Taller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab9parcial
+{
+    public class Taller
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+    }
+}
---
+++ Lab9parcial/alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab9parcial
+{
+    public class Alumno
+    {
+        public string Dpi { get; set; }
+        public string Nombre { get; set; }
+        public string Dirrecion { get; set; }
+    }
+}
